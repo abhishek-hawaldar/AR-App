e68e29a206593e0e2db0c50b9142152472c61358
abhishek-hawaldar <54952930+abhishek-hawaldar@users.noreply.github.com>
2020-07-26 17:58:27 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,102 @@
+package com.example.edi_project;
+
+import android.app.AlertDialog;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.core.HitResult;
+import com.google.ar.core.Plane;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ArFragment fragment;
+    private Uri selectedObject;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        fragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.sceneform_fragment);
+        InitializeGallery();
+
+        fragment.setOnTapArPlaneListener(
+                (HitResult hitResult, Plane plane, MotionEvent motionEvent) -> {
+                    if (plane.getType() != Plane.Type.HORIZONTAL_UPWARD_FACING) {
+                        return;
+                    }
+
+                    Anchor anchor = hitResult.createAnchor();
+                    placeObject(fragment, anchor, selectedObject);
+                }
+        );
+    }
+
+    private void InitializeGallery(){
+        LinearLayout gallery = findViewById(R.id.gallery_layout);
+
+        ImageView chair = new ImageView( this);
+        chair.setImageResource(R.drawable.chair_thumb);
+        chair.setContentDescription("chair");
+        chair.setOnClickListener(view -> {selectedObject = Uri.parse("chair.sfb");});
+        gallery.addView(chair);
+
+        ImageView couch = new ImageView( this);
+        couch.setImageResource(R.drawable.couch_thumb);
+        couch.setContentDescription("couch");
+        couch.setOnClickListener(view -> {selectedObject = Uri.parse("couch.sfb");});
+        gallery.addView(couch);
+
+        ImageView woodchair = new ImageView( this );
+        woodchair.setImageResource(R.drawable.woodchair_thumb);
+        woodchair.setContentDescription("woodchair");
+        woodchair.setOnClickListener(view -> {selectedObject = Uri.parse("woodchair.sfb");});
+        gallery.addView(woodchair);
+
+        ImageView sofa = new ImageView( this );
+        sofa.setImageResource(R.drawable.sofa_thumb);
+        sofa.setContentDescription("sofa");
+        sofa.setOnClickListener(view -> {selectedObject = Uri.parse("sofa.sfb");});
+        gallery.addView(sofa);
+
+        ImageView table = new ImageView( this );
+        table.setImageResource(R.drawable.table_thumb);
+        table.setContentDescription("table");
+        table.setOnClickListener(view -> {selectedObject = Uri.parse("table.sfb");});
+        gallery.addView(table);
+    }
+
+    private void placeObject(ArFragment fragment, Anchor anchor, Uri model){
+        ModelRenderable.builder()
+                .setSource(fragment.getContext(), model)
+                .build()
+                .thenAccept(renderable -> addNodeToScene(fragment, anchor, renderable))
+                .exceptionally((throwable -> {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setMessage(throwable.getMessage())
+                            .setTitle("Error");
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                    return null;
+                }));
+    }
+
+    private void addNodeToScene(ArFragment fragment, Anchor anchor, Renderable renderable){
+        AnchorNode anchorNode = new AnchorNode(anchor);
+        TransformableNode node = new TransformableNode(fragment.getTransformationSystem());
+        node.setRenderable(renderable);
+        node.setParent(anchorNode);
+        fragment.getArSceneView().getScene().addChild(anchorNode);
+        node.select();
+    }
+}
